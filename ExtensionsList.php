63d1abdcf60d56a52c4c89b83a52377141af7a2f
Vojtech Dobes <me@vojtechdobes.com>
2012-07-30 17:26:50 +0200

    Initial commit

---
+++ ExtensionsList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VojtechDobes;
+
+use Nette;
+use Nette\Config;
+
+
+/**
+ * Enables registration of other extensions in config file
+ *
+ * @author  Vojtěch Dobeš
+ * @license New BSD
+ */
+class ExtensionsList extends Config\CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$container = $this->getContainerBuilder();
+		$config = $this->getConfig();
+
+		foreach ($config as $name => $class) {
+			$this->compiler->addExtension($name, new $class);
+		}
+	}
+
+}
